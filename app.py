aaafee50ad042af104ff2cd8255f64ba0aba83b0
ragnaar12 <houssembenahmed11@gmail.com>
2025-04-27 13:05:37 +0100

    app.py

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Liste des commentaires
+# Liste pour stocker les commentaires
 comments = []
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
-        username = request.form['username']
-        comment = request.form['comment']
-        comments.append({'username': username, 'comment': comment})
-        return redirect('/')
-    return render_template('index.html', comments=comments)
+    # Si la méthode est POST, cela signifie que le formulaire a été soumis
+    if request.method == "POST":
+        username = request.form["username"]
+        comment = request.form["comment"]
+        
+        # Ajouter le commentaire à la liste
+        comments.append({"username": username, "comment": comment})
+    
+    # Afficher la page avec les commentaires
+    return render_template("index.html", comments=comments)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    # Lancer l'application en mode debug
     app.run(debug=True)
 
 
