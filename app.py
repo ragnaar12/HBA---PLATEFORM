efa33e454876a4d41156611d85e785dda48dcdb5
ragnaar12 <houssembenahmed11@gmail.com>
2025-04-27 12:48:22 +0100

    app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+comments = []
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        username = request.form['username']
+        comment = request.form['comment']
+        comments.append({'username': username, 'comment': comment})
+        return redirect('/')
+    return render_template('index.html', comments=comments)
+
+if __name__ == '__main__':
+    app.run(debug=True)
